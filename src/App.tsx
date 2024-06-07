9c06607995dc4252d39b46125e45b31de41ce8d2
SharkEzz <icraft640@gmail.com>
2024-06-07 22:27:24 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,12 @@ function App() {
   const engineRef = useRef<Engine>();
   const buttonRef = useRef<HTMLButtonElement>(null);
   const [dateDiff, setDateDiff] = useState(FINAL_DATE.getTime() - Date.now());
+  const [clickCount, setClickCount] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setDateDiff(FINAL_DATE.getTime() - Date.now());
-    }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  useEffect(() => {
-    const world = engineRef.current?.world;
-    if (!world) return;
-
-    const interval = setInterval(() => {
       const value = Common.random(0, 10);
       if (value < 5) return;
 
@@ -36,7 +26,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (!canvasRef.current) return;
+    if (!canvasRef.current || !buttonRef.current) return;
 
     const engine = Engine.create();
     engineRef.current = engine;
@@ -55,29 +45,21 @@ function App() {
       },
     });
 
-    const bodies = [];
+    const buttonPos = buttonRef.current.getBoundingClientRect();
 
-    const buttonPos = buttonRef.current?.getBoundingClientRect();
-    if (buttonPos) {
-      bodies.push(
-        Bodies.rectangle(
-          buttonPos.x + buttonPos.width / 2,
-          buttonPos.y + buttonPos.height / 2,
-          buttonPos.width,
-          buttonPos.height,
-          { isStatic: true, render: { visible: false } },
-        ),
-      );
-    }
-
-    bodies.push(
+    World.add(engine.world, [
+      Bodies.rectangle(
+        buttonPos.x + buttonPos.width / 2,
+        buttonPos.y + buttonPos.height / 2,
+        buttonPos.width,
+        buttonPos.height,
+        { isStatic: true, render: { visible: false } },
+      ),
       Bodies.rectangle(cw / 2, ch - 3, cw, 5, {
         isStatic: true,
         render: { visible: false },
       }),
-    );
-
-    World.add(engine.world, bodies);
+    ]);
 
     Render.run(render);
 
@@ -106,6 +88,11 @@ function App() {
     );
   };
 
+  const handleButtonClick = () => {
+    setClickCount((prev) => prev + 1);
+    handleAddAlbum();
+  };
+
   return (
     <>
       <canvas className="absolute left-0 top-0 z-0 pointer-events-none" ref={canvasRef} />
@@ -121,10 +108,16 @@ function App() {
         <p>{Math.floor(dateDiff / (1024 * 60 * 60))} heures</p>
         <p>{Math.floor(dateDiff / (1024 * 60))} minutes</p>
         <p>{Math.floor(dateDiff / 1024)} secondes</p>
-        <button ref={buttonRef} onClick={handleAddAlbum} className="py-3 px-4 mt-4 bg-slate-700 rounded">
+        <button ref={buttonRef} onClick={handleButtonClick} className="py-3 px-4 mt-4 bg-slate-700 rounded">
           Donne 😢
         </button>
       </div>
+      {clickCount > 10 && (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-800">
+          <img src="/feuneu.jpeg" />
+          <p className="text-3xl text-center">Sois patient mon reuf</p>
+        </div>
+      )}
     </>
   );
 }
