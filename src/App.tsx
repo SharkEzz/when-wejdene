0209fad78d6100030f70d47b521916c27b37d818
Tristan DIDA <tristan.dida@jvs.fr>
2024-06-10 15:43:14 +0200

    chore: updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Bodies, Common, Engine, Render, Runner, World } from 'matter-js';
+import { Bodies, Common, Engine, World } from 'matter-js';
 import { useEffect, useRef, useState } from 'react';
+import { FallingAlbums } from './FallingAlbums';
 
 const FINAL_DATE = new Date(2024, 6, 6);
 
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
   const engineRef = useRef<Engine>();
   const buttonRef = useRef<HTMLButtonElement>(null);
   const [dateDiff, setDateDiff] = useState(FINAL_DATE.getTime() - Date.now());
@@ -22,57 +22,6 @@ function App() {
 
     return () => {
       clearInterval(interval);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!canvasRef.current || !buttonRef.current) return;
-
-    const engine = Engine.create();
-    engineRef.current = engine;
-
-    const cw = window.innerWidth;
-    const ch = window.innerHeight;
-
-    const render = Render.create({
-      engine,
-      canvas: canvasRef.current,
-      options: {
-        width: cw,
-        height: ch,
-        background: 'transparent',
-        wireframes: false,
-      },
-    });
-
-    const buttonPos = buttonRef.current.getBoundingClientRect();
-
-    World.add(engine.world, [
-      Bodies.rectangle(
-        buttonPos.x + buttonPos.width / 2,
-        buttonPos.y + buttonPos.height / 2,
-        buttonPos.width,
-        buttonPos.height,
-        { isStatic: true, render: { visible: false } },
-      ),
-      Bodies.rectangle(cw / 2, ch - 3, cw, 5, {
-        isStatic: true,
-        render: { visible: false },
-      }),
-    ]);
-
-    Render.run(render);
-
-    const runner = Runner.create();
-    Runner.run(runner, engine);
-
-    Render.lookAt(render, { min: { x: 0, y: 0 }, max: { x: cw, y: ch } });
-
-    return () => {
-      Runner.stop(runner);
-      Render.stop(render);
-      World.clear(engine.world, false);
-      Engine.clear(engine);
     };
   }, []);
 
@@ -95,7 +44,7 @@ function App() {
 
   return (
     <>
-      <canvas className="absolute left-0 top-0 z-0 pointer-events-none" ref={canvasRef} />
+      <FallingAlbums buttonRef={buttonRef} engineRef={engineRef} />
       <div className="h-full w-full flex flex-col items-center justify-center gap-4">
         <h1 className="text-4xl">
           When{' '}
---
+++ src/FallingAlbums.tsx
@@ -0,0 +1,67 @@
+import { Bodies, Engine, Render, Runner, World } from 'matter-js';
+import { memo, useEffect, useRef, type MutableRefObject, type RefObject } from 'react';
+
+interface Props {
+  engineRef: MutableRefObject<Engine | undefined>;
+  buttonRef: RefObject<HTMLButtonElement | undefined>;
+}
+
+function FallingAlbumsComponent({ engineRef, buttonRef }: Props) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current || !buttonRef.current) return;
+
+    const engine = Engine.create();
+    engineRef.current = engine;
+
+    const cw = window.innerWidth;
+    const ch = window.innerHeight;
+
+    const render = Render.create({
+      engine,
+      canvas: canvasRef.current,
+      options: {
+        width: cw,
+        height: ch,
+        background: 'transparent',
+        wireframes: false,
+      },
+    });
+
+    const buttonPos = buttonRef.current.getBoundingClientRect();
+
+    World.add(engine.world, [
+      Bodies.rectangle(
+        buttonPos.x + buttonPos.width / 2,
+        buttonPos.y + buttonPos.height / 2,
+        buttonPos.width,
+        buttonPos.height,
+        { isStatic: true, render: { visible: false } },
+      ),
+      Bodies.rectangle(cw / 2, ch - 3, cw, 5, {
+        isStatic: true,
+        render: { visible: false },
+      }),
+    ]);
+
+    Render.run(render);
+
+    const runner = Runner.create();
+    Runner.run(runner, engine);
+
+    Render.lookAt(render, { min: { x: 0, y: 0 }, max: { x: cw, y: ch } });
+
+    return () => {
+      Runner.stop(runner);
+      Render.stop(render);
+      World.clear(engine.world, false);
+      Engine.clear(engine);
+    };
+  }, [buttonRef, engineRef]);
+
+  return <canvas className="absolute left-0 top-0 z-0 pointer-events-none" ref={canvasRef} />;
+}
+
+export const FallingAlbums = memo(FallingAlbumsComponent);
+FallingAlbums.displayName = 'FallingAlbums';
